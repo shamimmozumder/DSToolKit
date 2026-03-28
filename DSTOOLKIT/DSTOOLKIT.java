0ceda4bb9361012aa1a8e81875e9ebcb5daa615d
shamimmozumder <mozumdershamim123@gmail.com>
2026-03-28 23:47:01 +0600

    DSTOOLKIT.java

--- DSTOOLKIT/DSTOOLKIT.java
+++ DSTOOLKIT/DSTOOLKIT.java
@@ -1,19 +1,12 @@
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Font;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Cursor;
 
 public class DSTOOLKIT extends JFrame {
+
     private Container c;
     private Font f;
     private JButton linkedlistButton,stackButton,queueButton,bstButton,backButton;
@@ -21,43 +14,50 @@ public class DSTOOLKIT extends JFrame {
 
     DSTOOLKIT()
     {
-        
+
         setSize(1550, 900);
         setTitle("DSTOOLKIT");
         setLayout(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
+
+        // Background image set as content pane
         ImageIcon emg = new ImageIcon("pic.jpg");
         Image img1 = emg.getImage();
         Image temp_img1 = img1.getScaledInstance(1550,900,Image.SCALE_SMOOTH);
         emg = new ImageIcon(temp_img1);
-       JLabel background = new JLabel("", emg,JLabel.CENTER);
-        background.setBounds(0,0,1550,900);
-        add(background);
 
-        setVisible(true);
+        JLabel background = new JLabel(emg);
+        background.setLayout(null);
+
+        setContentPane(background);
 
         initComponents();
+
+        setVisible(true);
     }
+
+
     public void initComponents()
     {
+
         c = this.getContentPane();
-        c.setLayout(null);
 
         f = new Font("Arial",Font.BOLD + Font.ITALIC,22);
-         //Cursor making
-         cursor = new Cursor(Cursor.HAND_CURSOR);
 
-         //0-Back Button
-         backButton = new JButton("Back");
-         backButton.setFont(f);
-         backButton.setBounds(1200,650,200,50);
-         backButton.setForeground(Color.ORANGE);
-         backButton.setBackground(Color.RED);
-         backButton.setCursor(cursor);
-         c.add(backButton);
+        cursor = new Cursor(Cursor.HAND_CURSOR);
 
-        //1-Linkedlist
+
+        // Back Button
+        backButton = new JButton("Back");
+        backButton.setFont(f);
+        backButton.setBounds(1200,650,200,50);
+        backButton.setForeground(Color.ORANGE);
+        backButton.setBackground(Color.RED);
+        backButton.setCursor(cursor);
+        c.add(backButton);
+
+
+        // Linkedlist Button
         linkedlistButton = new JButton("LINKED-LIST");
         linkedlistButton.setFont(f);
         linkedlistButton.setBounds(700,200,200,50);
@@ -66,17 +66,18 @@ public class DSTOOLKIT extends JFrame {
         linkedlistButton.setCursor(cursor);
         c.add(linkedlistButton);
 
-         //2-STACK
-         stackButton = new JButton("STACK");
-         stackButton.setFont(f);
-         stackButton.setBounds(700,300,200,50);
-         stackButton.setForeground(Color.RED);
-         stackButton.setBackground(Color.WHITE);
-         stackButton.setCursor(cursor);
-         c.add(stackButton);
+
+        // Stack Button
+        stackButton = new JButton("STACK");
+        stackButton.setFont(f);
+        stackButton.setBounds(700,300,200,50);
+        stackButton.setForeground(Color.RED);
+        stackButton.setBackground(Color.WHITE);
+        stackButton.setCursor(cursor);
+        c.add(stackButton);
 
 
-          //3-QUEUE
+        // Queue Button
         queueButton = new JButton("QUEUE");
         queueButton.setFont(f);
         queueButton.setBounds(700,400,200,50);
@@ -85,83 +86,71 @@ public class DSTOOLKIT extends JFrame {
         queueButton.setCursor(cursor);
         c.add(queueButton);
 
-         //4-BINARY SEARCH TREE
-         bstButton = new JButton("BST");
-         bstButton.setFont(f);
-         bstButton.setBounds(700,500,200,50);
-         bstButton.setForeground(Color.RED);
-         bstButton.setBackground(Color.WHITE);
-         bstButton.setCursor(cursor);
-         c.add(bstButton);
 
-       bstButton.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e)
-        {
-            if(e.getSource()==bstButton)
-            {
-                dispose();
-                BSTVisualization BST = new BSTVisualization();
-                BST.setVisible(true);
-                
-            }
-            
-        }
-       });
+        // BST Button
+        bstButton = new JButton("BST");
+        bstButton.setFont(f);
+        bstButton.setBounds(700,500,200,50);
+        bstButton.setForeground(Color.RED);
+        bstButton.setBackground(Color.WHITE);
+        bstButton.setCursor(cursor);
+        c.add(bstButton);
 
-       stackButton.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent evt)
-        {
-            if(evt.getSource()== stackButton)
-            {
-                dispose();
-                StackVisualization SV = new StackVisualization();
-                SV.setVisible(true);
-            }
-        }
-       });
 
-       queueButton.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent evt)
-        {
-            if(evt.getSource()== queueButton)
-            {
-                dispose();
-                myQueue mq = new myQueue();
-                mq.setVisible(true);
-            }
-        }
-       });
+        // Action Listeners
 
-       backButton.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent evt)
-        {
-            if(evt.getSource()== backButton)
-            {
-                dispose();
-                frontPage sm = new frontPage();
-                sm.setVisible(true);
-            }
-        }
-       });
+        bstButton.addActionListener(e -> {
 
-       linkedlistButton.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent evt)
-        {
-            if(evt.getSource()== linkedlistButton)
-            {
-                dispose();
-                Llistfrontpage ab = new Llistfrontpage();
-                ab.setVisible(true);
-            }
-        }
-       });
+            dispose();
+            BSTVisualization BST = new BSTVisualization();
+            BST.setVisible(true);
 
-       
+        });
+
+
+        stackButton.addActionListener(e -> {
+
+            dispose();
+            StackVisualization SV = new StackVisualization();
+            SV.setVisible(true);
+
+        });
+
+
+        queueButton.addActionListener(e -> {
+
+            dispose();
+            myQueue mq = new myQueue();
+            mq.setVisible(true);
+
+        });
+
+
+        backButton.addActionListener(e -> {
+
+            dispose();
+            frontPage sm = new frontPage();
+            sm.setVisible(true);
+
+        });
+
+
+        linkedlistButton.addActionListener(e -> {
+
+            dispose();
+            Llistfrontpage ab = new Llistfrontpage();
+            ab.setVisible(true);
+
+        });
+
     }
-   
-    public static void main(String[] args) {
-         DSTOOLKIT frame =  new DSTOOLKIT();
-        
+
+
+    public static void main(String[] args)
+    {
+
+        new DSTOOLKIT();
+
     }
-    
+
 }
